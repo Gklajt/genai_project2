57e9b75f79c91de09981b20028f06b33792b39d1
Gokul Ajith <47082808+Gklajt@users.noreply.github.com>
2024-07-06 06:37:55 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+
+    name = "generative ai project 2",
+    version = '0.0.0',
+    author = "Gokul A",
+    author_email ="gkl.ajt@gmail.com",
+    packages = find_packages(),
+    install_required=[]
+)
